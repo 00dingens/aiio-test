11272058457588fb3584034167f8bf5c6ec725f7
Rafael Friesen <r.friesen@3dqr.de>
2024-01-23 15:39:48 +0100

    BE: get and post endpoint

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,8 +1,19 @@
 from django.shortcuts import render
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
+from .models import Product
+from .serializers import ProductSerializer
 
 @api_view(['GET'])
-def getData(request):
-  product = {'id': 1, 'productName': 'Product'}
-  return Response(product)
+def getProducts(request):
+  products = Product.objects.all()
+  serializer = ProductSerializer(products, many=True)
+  return Response(serializer.data)
+
+
+@api_view(['POST'])
+def addProduct(request):
+  serializer = ProductSerializer(data=request.data)
+  if serializer.is_valid():
+    serializer.save()
+  return Response(serializer.data)
