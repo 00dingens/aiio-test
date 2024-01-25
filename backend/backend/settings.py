c8289141a7336f983c14b45af06276baf05f1007
Rafael Friesen <r.friesen@3dqr.de>
2024-01-25 16:45:23 +0100

    Allow CORS for dev

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -40,9 +40,11 @@ INSTALLED_APPS = [
 
     'rest_framework',
     'api',
+    "corsheaders",
 ]
 
 MIDDLEWARE = [
+    "corsheaders.middleware.CorsMiddleware",
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -128,3 +130,10 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # Serve React app from build folder
 REACT_APP_BUILD_PATH = "../frontend/build"
+
+# Allow CORS for dev:
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000",
+    "http://127.0.0.1:8000",
+]
